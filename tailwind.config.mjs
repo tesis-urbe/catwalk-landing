28cc0e6da3a213aa6f13f3a9dccfde73fa4c4e53
nicolangelo damico <damiconicola98@gmail.com>
2024-10-21 19:20:32 -0400

    se agrego tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	colors: {
+		'blue': '#1fb6ff',
+		'purple': '#7e5bef',
+		'pink': '#ff49db',
+		'orange': '#ff7849',
+		'green': '#13ce66',
+		'yellow': '#ffc82c',
+		'gray-dark': '#273444',
+		'gray': '#8492a6',
+		'gray-light': '#d3dce6',
+		'catwalk-primary': '#900C3F',
+		'catwalk-secondary': '#DAF7A6'
+	},
+	plugins: [],
+}
